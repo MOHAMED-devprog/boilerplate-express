36d1122a9b8b6a924404984ea61d47650bf14d97
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-03-02 11:47:57 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ let absolutePath = __dirname + '/views/index.html';
 app.get('/',function(req , res){
    res.sendFile(absolutePath);
 });
+let path = __dirname + '/public' ;
+app.use(express.static(path));
 
 
 
