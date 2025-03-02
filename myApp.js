7e7cf9463f7e7d3d8dd73e7d98d8cf1998e4bca1
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-03-02 12:57:00 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 let dotenv = require('dotenv').config();
 let express = require("express");
 let app = express();
+
+
+app.use(function middleware(req , res , next){
+   console.log(`${req.method} ${req.path} - ${req.ip}`);
+   next();
+});
+
+
 let absolutePath = __dirname + '/views/index.html';
 app.get('/',function(req , res){
    res.sendFile(absolutePath);
@@ -19,10 +27,7 @@ app.get('/json', function(req , res){
       });
 });
 
-app.use(function(req , res , next){
-   console.log(`${req.method} ${req.path} - ${req.ip}`);
-   next();
-});
+
 
 
 
